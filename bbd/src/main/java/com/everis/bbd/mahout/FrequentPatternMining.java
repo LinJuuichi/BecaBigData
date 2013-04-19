b249d64aea378fc7dac15d425897950e16da6d1d
Foxline <skyline29@gmail.com>
2013-04-19 11:36:33 +0200

    Finished Mahout Frequent Pattern Mining implementation.

--- bbd/src/main/java/com/everis/bbd/mahout/FrequentPatternMining.java
+++ bbd/src/main/java/com/everis/bbd/mahout/FrequentPatternMining.java
@@ -1,20 +1,31 @@
 package com.everis.bbd.mahout;
 
-import java.nio.charset.Charset;
+import java.io.IOException;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.mahout.common.HadoopUtil;
 import org.apache.mahout.common.Pair;
+import org.apache.mahout.common.Parameters;
+import org.apache.mahout.fpm.pfpgrowth.PFPGrowth;
+import org.apache.mahout.fpm.pfpgrowth.convertors.string.TopKStringPatterns;
 
+import com.everis.bbd.utilities.ConfigurationReader;
+
+/**
+ * @author jsabatep
+ *
+ */
 public class FrequentPatternMining 
 {
-
+	
+	private static Logger log = Logger.getLogger(FrequentPatternMining.class.getName());
+	
+	private static String CONFIGURATION_PATH = "config/mahoutFPM.properties";
+	
 	private String _inputPath;
 	
 	private String _outputPath;
@@ -23,51 +34,90 @@ public class FrequentPatternMining
 	
 	private String _splitterPattern = " ";
 	
-	private Collection<?> _returnableFeatures; 
+	private String _minSupport = "3";
 	
-    public static void main( String[] args )
+	private String _maxHeapSize = "50";
+	
+	private Collection<String> _returnableFeatures; 
+	
+	private List<Pair<String,TopKStringPatterns>> _results;
+	
+    /**
+     * @param args
+     * @throws Exception 
+     */
+    public static void main( String[] args ) throws Exception
     {
        FrequentPatternMining fpm = new FrequentPatternMining();
        fpm.makeConfiguration();
-       fpm.runFPGrowth();
+       fpm.startPFPGrowth();
+       fpm.processResults();
+       fpm.writeResults();
     }
 	
     private void makeConfiguration() 
     {
-    	 ConfigurationReader cr = new ConfigurationReader(path);
+    	 ConfigurationReader cr = new ConfigurationReader(CONFIGURATION_PATH);
          cr.readConfigurationFile();
          
-         _inputPath = cr.getValue.get("inputPath");
-         _outputPath = cr.getValue.get("outputPath");
+         _inputPath = cr.getValue("inputPath");
+         _outputPath = cr.getValue("outputPath");
          _encoding = cr.getValue("encoding", _encoding);
          _splitterPattern = cr.getValue("splitterPattern", _splitterPattern);
          _returnableFeatures = cr.getValues("returnableFeatures");
-    }     
+         _minSupport = cr.getValue("minSupport", _minSupport);
+         _maxHeapSize = cr.getValue("maxPatternSupport", _maxHeapSize);
+    }    
     
-     
-    
-    private void runFPGrowth() 
+    private void startPFPGrowth() throws Exception 
     {
+    	Parameters params = new Parameters();
+    	
+    	params.set("minSupport", _minSupport);
+    	params.set("maxHeapSize", _maxHeapSize);
+    	params.set("splitPattern", _splitterPattern);
+    	params.set("encoding", _encoding);
+    
     	Path input = new Path(_inputPath);
     	Path output = new Path(_outputPath);
-    	FSDataInputStream inputStream = null;
-        Configuration conf = new Configuration();
-        FileSystem fs = FileSystem.get(output.toUri(), conf);
-        Charset encoding = Charset.forName(_encoding);
-    	Iterator<Pair<List<?>, Long>> transactionStream;
     	
-        new StringRecordIterator(new FileLineIterable(inputStream, encoding, false), pattern),
-
-        
-        
-        
-        
-        int maxHeapSize = Integer.valueOf(params.get("maxHeapSize", "50"));
-        int minSupport = Integer.valueOf(params.get("minSupport", "3"));
-        
-        
-        FileSystem fs = FileSystem.get(output.toUri(), conf);
-   }
+    	params.set("input", input.toString());
+        params.set("output", output.toString());
+    	
+    	Configuration conf = new Configuration();
+    	HadoopUtil.delete(conf, output);
+    	
+    	PFPGrowth.runPFPGrowth(params);
+    }
+    
+    private void processResults() throws IOException
+    {
+    	Parameters params = new Parameters();
+    	Path output = new Path(_outputPath);
+    	params.set(PFPGrowth.OUTPUT, output.toString());
+    	
+    	_results = PFPGrowth.readFrequentPattern(params);
+    	
+    	for(Pair<String,TopKStringPatterns> p : _results)
+    	{
+    		boolean exists = false;
+    		for(String s : _returnableFeatures) 
+    		{
+    			if(p.getFirst().equalsIgnoreCase(s)) 
+    			{
+    				exists = true;
+    				break;
+    			}
+    		}
+    		if(!exists) _results.remove(p);
+    	}
+    } 
+    
+    private void writeResults() {
+    	for(Pair<String,TopKStringPatterns> p : _results)
+    	{
+    		log.info(p.getFirst() + " " + p.swap().toString());
+    	}
     }
     
 }
