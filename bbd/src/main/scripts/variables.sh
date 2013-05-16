15632a1f8fad4c78844efc2e72a43c29241f0bc0
Foxline <skyline29@gmail.com>
2013-05-16 12:19:00 +0200

    Fixed a bug in some shells scripts.

--- bbd/src/main/scripts/variables.sh
+++ bbd/src/main/scripts/variables.sh
@@ -1,9 +1,11 @@
-#Application version.
-BBD_VERSION=bbd-0.0.1-SNAPSHOT
 #Application install directory.
 BBD_INSTALL_DIR=/opt/bbd
+#Application version.
+BBD_VERSION=bbd-0.0.1-SNAPSHOT
 #BBD link.
 BBD_LINK=/usr/bin/bbd
+#BBD version link.
+BBD_VERSION_LINK="${BBD_INSTALL_DIR}/bbd"
 #Flume-ng install directory.
 FLUME_INSTALL_DIR=/opt/flume
 #Mahout install directory.
